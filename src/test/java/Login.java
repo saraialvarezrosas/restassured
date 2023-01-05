4f4ad6a47cc017d05b57761253d6b83a94b29805
Sarai Alvarez <Sarai.Alvarez@jala.university>
2023-01-05 16:39:28 -0400

    Create class login

---
+++ src/test/java/Login.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class Login {
+    @Test
+    public void logintest() {
+        String responsee =  RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"username\": \"sarai1234\",\n" +
+                        "    \"password\": \"1234sarai\"\n" +
+                        "}")
+                .post("https://test-api.k6.io/auth/basic/login/")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .asString();
+        System.out.println(responsee);
+    }
+}
